41122e15de815b3417251117dae100956aca3a4e
Asaduzzaman9202 <asaduzzaman8316@gmail.com>
2025-11-19 00:41:40 +0600

    update env file

--- app/DataFetch/useCourse.jsx
+++ app/DataFetch/useCourse.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 
 function useCourse() {
     const [course, setCategory] = useState([])
-    const baseurl = process.env.NEXT_PUBLIC_BASE_URL
+    const baseurl = process.env.NEXT_PUBLIC_BASE_URL 
     useEffect(() => {
         async function getData() {
             const res = await fetch(`${baseurl}/data.json`)
