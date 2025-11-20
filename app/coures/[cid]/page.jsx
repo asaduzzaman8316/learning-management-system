5ec15e720e43b30392093d32ab26d4be50f8514e
Asaduzzaman9202 <asaduzzaman8316@gmail.com>
2025-11-21 01:56:29 +0600

    single course responsive

--- app/coures/[cid]/page.jsx
+++ app/coures/[cid]/page.jsx
@@ -15,31 +15,31 @@ async function page({ params }) {
     return (
         <div className='w-full lg:w-[70%] mx-auto px-4 py-20'>
             <div className=' grid grid-cols-12 gap-8'>
-                <div data-aos='fade-up-right' data-aos-duration='1500' className='border border-gray-200 rounded-2xl shadow-sm col-span-9 p-7'>
-                    <Image src={course.course_image} width={1000} height={400} alt='course image' className=' rounded-md' />
+                <div data-aos='fade-up-right' data-aos-duration='1500' className='border border-gray-200 rounded-2xl shadow-sm col-span-12 lg:col-span-9 p-7'>
+                    <Image src={course.course_image} width={1000} height={400} alt='course image' className=' rounded-md ' />
                     <div className='flex items-center gap-6 py-6 '>
                         <p className='text-white bg-[#07a698] w-fit px-5 rounded-full py-0.5'>Best Seler</p>
                         <p className='text-white bg-[#07a698] w-fit px-5 rounded-full py-0.5'>Latest</p>
                     </div>
                     <h1 className='text-4xl font-semibold text-gray-800'>{course.course_title}</h1>
 
-                    <div className='flex items-center divide-gray-200 divide-x justify-between py-6'>
-                        <div className='flex items-center gap-3 pr-4'>
+                    <div className='grid grid-cols-2 gap-6 lg:grid-cols-4 lg:gap-0 items-center divide-gray-200 divide-x justify-between py-6'>
+                        <div className='flex items-center gap-3 '>
                             <Image src={course.instructor.image} width={70} height={100} alt='instructor image' className='h-auto w-auto' />
                             <p className='font-medium'><span className='text-[#07a698]'>Instructor: </span>{course.instructor.name}</p>
                         </div>
 
-                        <div className='flex items-center gap-3 pr-4 '>
+                        <div className='flex items-center gap-3  '>
                             <FaTags className='text-[#07a698]' />
                             <p className=' font-medium'>Web Development</p>
                         </div>
 
-                        <div className='flex items-center gap-3 pr-4 '>
+                        <div className='flex items-center gap-3  '>
                             <CiCalendar className='text-[#07a698] text-xl' />
                             <p className=' font-medium'>20 November 2025</p>
                         </div>
 
-                        <div className='flex items-center gap-3 pr-4 '>
+                        <div className='flex items-center gap-3  '>
                             <div className='flex items-center gap-2'>
                                 <FaRegStar className='text-[#07a698] text-lg' />
                                 <FaRegStar className='text-[#07a698] text-lg' />
@@ -52,7 +52,7 @@ async function page({ params }) {
 
                     </div>
                 </div>
-                <div data-aos='fade-up-left' data-aos-duration='1500' className=' col-span-3'>
+                <div data-aos='fade-up-left' data-aos-duration='1500' className=' lg:col-span-3 col-span-12'>
                     <AddCart price={course.price} />
                     <CourseInfo course={course} />
                 </div>
